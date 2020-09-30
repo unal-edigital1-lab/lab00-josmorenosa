a4f635b4575cbaec978f5c3f6e4fa315bef4909d
josmorenosa <31423031+josmorenosa@users.noreply.github.com>
2020-09-30 08:54:26 -0500

    Update sum1bcc.v

--- src/sum1bcc.v
+++ src/sum1bcc.v
@@ -1,11 +1,14 @@
-module sum1bcc (A, B, Ci,Cout,S);
-
+module sum1bcc (A, B, Ci,Cout,S); //NOMBRE DEL MÓDULO
+   
+  //DEFINIMOS ENTRADAS
   input  A;
   input  B;
   input  Ci;
+  //DEFINIMOS SALIADAS
   output Cout;
   output S;
-
+  
+  //ALMACENAMOS LOS DATOS EN UN REGISTRO
   reg [1:0] st;
   assign S = st[0];
   assign Cout = st[1];
